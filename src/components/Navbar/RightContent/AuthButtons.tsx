ec276e3a5833638f26c621de47a492917df99971
KielBlondell <49730718+KielBlondell@users.noreply.github.com>
2023-03-02 19:52:48 -0400

    Commit @ Mar-2-2023

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -21,7 +21,7 @@ const AuthButtons: React.FC = () => {
         height="28px"
         display={{ base: "none", sm: "flex" }}
         width={{ base: "70px", md: "110px" }}
-        mr={2}
+        mb={2}
         onClick={() => setAuthModalState({ open: true, view: "signup" })}
       >
         Sign Up
